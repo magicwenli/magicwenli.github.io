d955fbc73a9c178b59509e7e005be097154cbc12
magicwenli <yxra3603@outlook.com>
2021-04-01 12:26:30 +0800

    add animation

--- _includes/scripts/page.js
+++ _includes/scripts/page.js
@@ -1,3 +1,26 @@
 /*(function () {
 
 })();*/
+
+const a = document.querySelector('a')
+// a.innerHTML = a.textContent.replace(/\S/g, "<span>$&</span>")
+
+// Support Space:
+a.innerHTML = a.textContent
+  .replace(/\S/g, "<span>$&</span>")
+  .replace(/\s/g, "<span>&nbsp;</span>")
+
+let delay = 0
+document.querySelectorAll('a span').forEach((span, index) => {
+  delay += 0.1
+  
+  if (index === 6) delay += 0.3
+  
+  span.style.setProperty('--delay', `${delay}s`)
+})
+
+a.addEventListener('animationend', (e) => {
+  if (e.target === document.querySelector('a span:last-child')) {
+    a.classList.add('ended')
+  }
+})
